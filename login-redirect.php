1a01d2254b4bc516a0f5c869734f389ac8ee3506
Md. Nazmul Hassan <nayemDevs@gmail.com>
2015-07-29 17:21:11 +0600

    Update login-redirect.php

--- login-redirect.php
+++ login-redirect.php
@@ -12,10 +12,23 @@ License: none
     add_filter('wpuf_login_redirect' , 'login_redirect');
 
         function login_redirect($redirect){
-                $redirect = home_url( '/dashboard' );
+                $redirect = home_url( '/get_option( 'menu_option' );' );
                 return $redirect;
         }
 
+    add_filter( 'wpuf_options_others', 'menu_option');
+
+    function menu_option( $settings_fields ){
+
+    	 $settings_fields =  array(
+                'name'    => 'page_url',
+                'label'   => __( 'Insert your URL ', 'wpuf' ),
+                'desc'    => __( 'Insert your URL to redirect user after login', 'wpuf' ),
+                'type'    => 'checkbox',
+                'default' => 'on'
+            ),
+
+   }
 
 
 ?>
